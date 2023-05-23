ebf653a6f0fac4b1f20813a9ff734df9fe2d3f8a
s2210239021 <s2210239021@fhooe.at>
2023-05-23 19:16:20 +0200

    1a

--- sal2_uez3/sal2_uez_a1.py
+++ sal2_uez3/sal2_uez_a1.py
@@ -1,4 +1,5 @@
 from collections import deque
+# used for popleft (receiving node from the front of the queue - not possible with lists)
 
 rooted_tree = [
         [1, 2, 3],      #0
@@ -16,21 +17,38 @@ rooted_tree = [
         [],             #12
 ]
 
+# 1 a.) breadth_first_search
 def breadth_first_search(rooted_tree, target_node):
-    visited = set()
-    queue = deque([(0, "")])
+    """
+    Perform breadth-first search on a rooted tree and print every visited node, highlighting the target node.
+
+    Args:
+        rooted_tree (list): A list representing the rooted tree structure.
+            Each element of the list represents a node, and the value of each element is a list of its neighbors.
+            The index of each element corresponds to the node's unique identifier.
+        target_node (int): The identifier of the target node to be highlighted in the printed output.
+
+    Returns:
+        None
+
+    """
+    visited = set()  # Set to keep track of visited nodes
+    queue = deque()  # Queue to store nodes for breadth-first traversal
+    queue.append(0)  # Starting from the root node (index 0)
 
     while queue:
-        node, path = queue.popleft()
-        visited.add(node)
+        current_node = queue.popleft()  # Retrieve the node from the front of the queue
+        visited.add(current_node)  # Mark the current node as visited
 
-        if node == target_node:
-            print(f"Visiting node {node} using breadth_first_search.")
+        if current_node == target_node:
+            print(f"[{current_node}]")  # Highlight the target node by enclosing it in square brackets
         else:
-            print(f"Found target node {node} using breadth_first_search.")
+            print(current_node)
 
-        for child in rooted_tree[node]:
-            if child not in visited:
-                queue.append((child, path + str(node) + " -> "))
+        neighbors = rooted_tree[current_node]  # Get the neighbors of the current node
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                queue.append(neighbor)  # Add the unvisited neighbor nodes to the queue for further traversal
+
 
 breadth_first_search(rooted_tree, 10)
