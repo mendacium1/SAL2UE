28256797314c3fa52bc2fe7f8618b3e650f20f3d
s2210239021 <s2210239021@fhooe.at>
2023-06-08 18:12:52 +0200

    Not working

--- sal2_uez4/sal2_uez4_a1.py
+++ sal2_uez4/sal2_uez4_a1.py
@@ -4,13 +4,17 @@ attacker_numbers = set()
 
 with open("call_log.txt", "r", encoding="utf-8") as call_log:
     for line in call_log:
-        match = re.search(r'\b0(\d{4,5})[./]{1,3}(?:1|4)(\d{7,9})\b', line)
+        match = re.search(r'', line)
         if match:
+            print(f"matched: {line}")
             area_code = match.group(1)
             number = match.group(2)
             formatted_number = f"{area_code}/{number}"
             attacker_numbers.add(formatted_number)
+        else:
+             print(f"no-match: {line}")
 
 with open("filtered_numbers.txt", "w", encoding="utf-8") as output_file:
     for number in attacker_numbers:
         output_file.write(number + "\n")
+
