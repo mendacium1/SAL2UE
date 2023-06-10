a8dea4fc56528aeabc229fb596b549a3ff2a0d17
s2210239021 <s2210239021@fhooe.at>
2023-06-10 22:09:56 +0200

    Update sal2_uez4_a1.py

--- sal2_uez4/sal2_uez4_a1.py
+++ sal2_uez4/sal2_uez4_a1.py
@@ -1,6 +1,11 @@
-"""Exercise sheet 4
+"""
+sal2_uez3_a1.py
+(c) S2210239021, Jakob Mayr
+Exercise sheet 4
 
-This python program reads in telephone numbers from a log-file called "call_log.txt". It than filters out numbers from "attackers" by a given ruleset using a regex. The attack numbers are then written into the file "attacker_numbers.txt".
+This python program reads in telephone numbers from a log-file called "call_log.txt". It than
+filters out numbers from "attackers" by a given ruleset using a regex. The attack numbers are then
+written into the file "attacker_numbers.txt".
 """
 import re
 
@@ -8,7 +13,15 @@ import re
 attacker_numbers = set()
 
 # Regular expression pattern to match attacker phone numbers
-attacker_phone_number_regex = re.compile(r'\b0(\d{4,5})(?:[\/]{1,3}|\.)\d\d[1|4](\d{5,7})\b')
+# Explanation:
+# \b - word boundary anchor (so that match does not occur in larger word)
+# 0 - String begins with 0
+# (\d{4,5}) - 4 or 5 digits long area code (brackets for "area"-group)
+# (?:[\/]{1,3}|\.) - Non-capturing group that looks for "/", "//", "///" or a ".". This represents
+# the seperator between area-code and number
+# (\d\d[1|4]\d{5,7}) - Group that checks the number. (2 digits followed by 1 or 3 in the thir
+# place and 5-7 digits at the end - total length 8-10)
+attacker_phone_number_regex = re.compile(r'\b0(\d{4,5})(?:[\/]{1,3}|\.)(\d\d[1|4]\d{5,7})\b')
 
 # Open the call log file to read in phone numbers
 with open("call_log.txt", "r", encoding="utf-8") as call_log:
